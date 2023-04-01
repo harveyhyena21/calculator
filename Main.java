674864ad1977e811a9c05c3d2c2fbf8552be5c22
harveyhyena21 <ohu02@ladueschools.net>
2023-04-01 01:19:04 +0000

    afsava

--- Main.java
+++ Main.java
@@ -9,14 +9,13 @@ public class Main{
     }
 
     public static double calculate(String s) {
-        int size = s.length();
         int index1 = 0;
         int index2 = 0;
 
         if (s.contains("(")) {
             index1 = s.indexOf("(");
             //starts from the end of the calculation to find the index of the end paranthesis
-            for (int i = size - 1; i >= 0; i--) {
+            for (int i = s.length() - 1; i >= 0; i--) {
                 if (s.substring(i, i + 1).equals(")")) {
                     index2 = i;
                     break;
@@ -26,7 +25,7 @@ public class Main{
         }
 
         index1 = 0;
-        index2 = size;
+        index2 = s.length();
 
 
         if (s.contains("^")) {
@@ -42,7 +41,7 @@ public class Main{
                     }
                 }
             }
-            for (int i = carrotIndex + 1; i < size; i++) {
+            for (int i = carrotIndex + 1; i < s.length(); i++) {
                 try {
                     Integer.parseInt(s.substring(i, i + 1));
                 } catch (Exception e) {
@@ -59,9 +58,9 @@ public class Main{
         }
 
         index1 = 0;
-        index2 = size;
+        index2 = s.length();
 
-        for (int j = 0; j < size; j++) {
+        for (int j = 0; j < s.length() - 1; j++) {
             if (s.substring(j, j + 1).equals("*")) {
                 int starIndex = s.indexOf("*");
                 for (int i = starIndex - 1; i >= 0; i--) {
@@ -75,7 +74,8 @@ public class Main{
                         }
                     }
                 }
-                for (int i = starIndex + 1; i < size; i++) {
+
+                for (int i = starIndex + 1; i < s.length(); i++) {
                     try {
                         Integer.parseInt(s.substring(i, i + 1));
                     } catch (Exception e) {
@@ -89,10 +89,11 @@ public class Main{
 
                 double answer = Double.parseDouble(s.substring(index1, starIndex)) * Double.parseDouble(s.substring(starIndex + 1, index2));
                 s = s.substring(0, index1) + answer + s.substring(index2);
+                j = index1;
             }
 
             index1 = 0;
-            index2 = size;
+            index2 = s.length();
 
             if (s.substring(j, j + 1).equals("/")) {
                 int divideIndex = s.indexOf("/");
@@ -107,7 +108,7 @@ public class Main{
                         }
                     }
                 }
-                for (int i = divideIndex + 1; i < size; i++) {
+                for (int i = divideIndex + 1; i < s.length(); i++) {
                     try {
                         Integer.parseInt(s.substring(i, i + 1));
                     } catch (Exception e) {
@@ -121,10 +122,81 @@ public class Main{
 
                 double answer = Double.parseDouble(s.substring(index1, divideIndex)) / Double.parseDouble(s.substring(divideIndex + 1, index2));
                 s = s.substring(0, index1) + answer + s.substring(index2);
+                j = index1;
             }
         }
 
 
+
+
+        index1 = 0;
+        index2 = s.length();
+
+        for (int j = 0; j < s.length() - 1; j++) {
+            if (s.substring(j, j + 1).equals("+")) {
+                int addIndex = s.indexOf("+");
+                for (int i = addIndex - 1; i >= 0; i--) {
+                    try {
+                        Integer.parseInt(s.substring(i, i + 1));
+                    } catch (Exception e) {
+                        if (s.substring(i, i + 1).equals("."));
+                        else {
+                            index1 = i + 1;
+                            break;
+                        }
+                    }
+                }
+
+                for (int i = addIndex + 1; i < s.length(); i++) {
+                    try {
+                        Integer.parseInt(s.substring(i, i + 1));
+                    } catch (Exception e) {
+                        if (s.substring(i, i + 1).equals("."));
+                        else {
+                            index2 = i;
+                            break;
+                        }
+                    }
+                }
+
+                double answer = Double.parseDouble(s.substring(index1, addIndex)) + Double.parseDouble(s.substring(addIndex + 1, index2));
+                s = s.substring(0, index1) + answer + s.substring(index2);
+                j = index1;
+            }
+
+            index1 = 0;
+            index2 = s.length();
+
+            if (s.substring(j, j + 1).equals("-")) {
+                int subIndex = s.indexOf("-");
+                for (int i = subIndex - 1; i >= 0; i--) {
+                    try {
+                        Integer.parseInt(s.substring(i, i + 1));
+                    } catch (Exception e) {
+                        if (s.substring(i, i + 1).equals(".")) index1 = i + 1;
+                        else {
+                            index1 = i + 1;
+                            break;
+                        }
+                    }
+                }
+                for (int i = subIndex + 1; i < s.length(); i++) {
+                    try {
+                        Integer.parseInt(s.substring(i, i + 1));
+                    } catch (Exception e) {
+                        if (s.substring(i, i + 1).equals(".")) index2 = i;
+                        else {
+                            index2 = i;
+                            break;
+                        }
+                    }
+                }
+
+                double answer = Double.parseDouble(s.substring(index1, subIndex)) - Double.parseDouble(s.substring(subIndex + 1, index2));
+                s = s.substring(0, index1) + answer + s.substring(index2);
+                j = index1;
+            }
+        }
 
         return Double.parseDouble(s);
     }
